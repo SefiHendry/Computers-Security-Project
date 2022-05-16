a0918178141fab34d1a7a74baa67f34131da516d
Adir Digmi <morisos06@gmail.com>
2022-05-16 20:57:36 +0300

    Make hashing unsafe by replacing bcrypt with sha3

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,34 +1,29 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
 const jwt = require("jsonwebtoken");
-const passwordConfig = require('../config');
+const passwordConfig = require("../config");
+const sha3_512 = require("js-sha3").sha3_512;
 
 router.post("/login", async (req, res) => {
-    const {password,email} = req.body
-    db.query("SELECT id,email,password,failedLoginAttempts FROM users where email=(?)",
-        [email], (error, results, fields) => {
-            if (error) {
-                return res.status(500).send("An error occurred");
-            }
-            if (results.length === 0) {
-                return res.status(500).send("user email is incorrect");
-            }
-            if (results[0].failedLoginAttempts >= passwordConfig["maximum attempts"]) {
-                return res.status(500).send("your account has been disabled due to too many failed attempts");
-            }
-            bcrypt.compare(password, results[0].password, (bcryptError, bcryptResults) => {
-                if (bcryptError) {
-                    return res.status(500).send("An error occurred");
-                }
-                if (!bcryptResults) {
-                    db.query("UPDATE users SET failedLoginAttempts = failedLoginAttempts+1 WHERE email = (?)", [email]);
-                    return res.status(500).send("Password is incorrect");
-                }
-                db.query("UPDATE users SET failedLoginAttempts = 0 WHERE email = (?)", [email]);
-                token = jwt.sign({ user: results[0] }, process.env.TOKEN_KEY);
-                return res.json({ token });
-            });
-        });
-})
+  try {
+    const { password, email } = req.body;
+    const hashedPassword = sha3_512(password);
+    db.query(
+      `SELECT id,email,password,failedLoginAttempts FROM users where email = "${email}" and password = "${hashedPassword}"`,
+      (error, results, fields) => {
+        if (error) {
+          return res.status(500).send("An error occurred");
+        }
+        if (results.length == 0) {
+          return res.status(400).send("Email or password is incorrect");
+        }
 
-module.exports = router
+        token = jwt.sign({ user: results[0] }, process.env.TOKEN_KEY);
+        return res.json({ token });
+      }
+    );
+  } catch (error) {
+    return res.status(500).send("An error occurred");
+  }
+});
+
+module.exports = router;
